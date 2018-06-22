4c8cbac29a39cd5b466022ea68f2144c92466fbe
Bolean <wodeln@163.com>
2018-06-22 15:24:13 +0800

    mongo持久化

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express=require('express');
-
 var im =require('./routers/im.js');
+var mongoose = require('./mongo/mongoose.js');
+var db = mongoose();
 
 var app=new express();
 
-app.use('/im',im);
-app.use('/',im);
-app.listen(3001,function () {
+app.use('/im/v1',im);
+app.use('/v1',im);
+app.listen(3001,'is.com',function () {
     console.log("server started")
 });
--- routers/im/user.js
+++ routers/im/user.js
@@ -1,9 +1,24 @@
-var express=require('express');
+var express = require('express');
 var router = express.Router();
-
+var User = require('../../models/User');// 引入模型
 //  /admin/goods
-router.get('/',function(req,res){
+router.get('/', function (req, res) {
     res.send('user index');
+});
+
+router.get('/addUser', function (req, res) {
+    var user = new User({
+        username: 'admin',
+        password: '123'
+    });
+    user.save((err) => { //添加
+        if(!err){
+            res.send("添加成功");
+        }else{
+            console(err);
+        }
+        // console.log('save status:', err ? 'failed' : 'success');
+    });
 });
 
 module.exports = router;
