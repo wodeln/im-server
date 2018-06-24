424147e6c624bf5869c47c89e3b91008399b8847
bolean <wodeln@163.com>
2018-06-24 14:45:33 +0800

    设置静态目录

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express=require('express');
 var im =require('./routers/im.js');
+let path = require('path');
 var mongoose = require('./mongo/mongoose.js');
 var db = mongoose();
 
 var app=new express();
 
+app.use('/public',express.static(path.join(__dirname, 'public')));
 app.use('/im/v1',im);
 app.use('/v1',im);
 app.listen(3001,'is.com',function () {
