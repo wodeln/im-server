10fc481269e2cfa9f9b28a1373411d2724ae433c
bolean <wodeln@163.com>
2018-06-25 12:54:12 +0800

    token验证

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-var express=require('express');
-var im =require('./routers/im.js');
+let express=require('express');
+let im =require('./routers/im.js');
 let path = require('path');
-var mongoose = require('./mongo/mongoose.js');
-var db = mongoose();
-
-var app=new express();
+let mongoose = require('./mongo/mongoose.js');
+let tokenUtil = require('./util/token');
+let db = mongoose();
+let app=new express();
 
 app.use('/public',express.static(path.join(__dirname, 'public')));
+
+app.use(function (request,response,next) {
+    let token = request.get("Authorization");
+    if(!tokenUtil.checkToken(token)){
+        response.sendStatus(401);
+    }else {
+        next();
+    }
+});
+
 app.use('/im/v1',im);
 app.use('/v1',im);
 app.listen(3001,'is.com',function () {
