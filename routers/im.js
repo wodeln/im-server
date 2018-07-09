3e70babc69677eb5543e840381ffe462ee2897b6
bolean <wodeln@163.com>
2018-07-09 10:31:27 +0800

    关注路由

--- routers/im.js
+++ routers/im.js
@@ -2,6 +2,8 @@ var express=require('express');
 var router = express.Router();
 
 var user=require('./im/user.js');
+var following=require('./im/following.js');
 router.use('/user',user);
 router.use('/',user);
+router.use('/following',following);
 module.exports = router;
